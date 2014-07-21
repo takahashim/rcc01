662f24f19704e1c2325f110c26c5ae0366a2c9d2
takahashim <maki@rubycolor.org>
2014-07-21 15:27:41 +0900

    fix typo

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -16,14 +16,14 @@ class ApiController < ApplicationController
                         commit_commiter_name: params[:head_commit][:commiter][:name],
                         commit_author_username: params[:head_commit][:author][:username],
                         commit_commiter_username: params[:head_commit][:commiter][:username],
-                        commit_message: params[:head_commit][:message]
+                        commit_message: params[:head_commit][:message],
                         repository_name: params[:repository][:name],
                         repository_url: params[:repository][:url],
                         repository_description: params[:repository][:description],
                         repository_owner_name: params[:repository][:owner][:name],
                         repository_private: params[:repository][:private],
                         repository_master_branch: params[:repository][:master_branch],
-                        rawjson: params[:api].to_json,
+                        rawjson: params[:api].to_json
                         )
   end
 end
