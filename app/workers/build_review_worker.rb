9765c0ebcddaf676ca2071b3365d631ad9f872a1
takahashim <maki@rubycolor.org>
2014-07-24 17:30:48 +0900

    loggin docker output

--- app/workers/build_review_worker.rb
+++ app/workers/build_review_worker.rb
@@ -24,11 +24,12 @@ class BuildReviewWorker
     logger.info("build: user_id:#{user_id}, repository:#{repository_url}")
     args = Shellwords.shelljoin(["docker","run","--rm","-t","-i",DOCKER_ID,DOCKER_SCRIPT,repository_url,commit.commit_id,dropbox_token])
     logger.info("docker command:" + args.to_s)
-    output = `#{args} 2>&1`
+    dockerlog = `#{args} 2>&1`
+    logger.info("docker log:" + dockerlog)
     finished_at = Time.now
 
     @build.finished_at = finished_at
-    @build.log = output
+    @build.log = dockerlog
     @build.save
 
     logger.info("end")
